10d6878d4e8c720845fa08b0f178473d8b81f254
alumno <alumno@admins-MacBook-Pro.local>
2018-12-22 10:05:53 -0500

    Upload File

---
+++ Node/Clase12/handlers/images.handler.ts
@@ -0,0 +1,47 @@
+import multer = require("multer")
+import jimp = require("jimp")
+import uuid = require("uuid")
+import { IError } from "../interfaces/ierror.interface";
+import { Request, Response, NextFunction } from "express"
+
+const handlersImages = {
+	upload: () => {
+		const optionsMulter = {
+			storage: multer.memoryStorage(),
+			fileFilter: (req, file, next) => {
+				const isPhoto = file.mimetype.startsWith("image/")
+
+				if (isPhoto) {
+					return next(null, true)
+				}
+
+				const error: IError = new Error("The file is not a image")
+				error.status = 500
+
+				next(error, false)
+			}
+		}
+
+		return multer(optionsMulter).single("photo")
+	},
+
+	save: () => {
+
+		return async (req: Request, res: Response, next: NextFunction) => {
+			if (!req.file) return next()
+
+			const extension: string = req.file.mimetype.split("/")[1]
+			const name: string = uuid.v4()
+
+			req.body.photo = `${name}.${extension}`
+
+			const foto = await jimp.read(req.file.buffer)
+			await foto.resize(300, jimp.AUTO)
+			await foto.write(`./public/uploads/${req.body.photo}`)
+
+			next()
+		}
+	}
+}
+
+export { handlersImages }
